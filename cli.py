77af2411c7eaf6e5f582f52e6717b5fe878ed3a2
Metaray <metaray21@gmail.com>
2022-06-21 18:03:22 +0300

    Add proper export to CSV functionality

--- cli.py
+++ cli.py
@@ -1,5 +1,4 @@
 import argparse
-import traceback
 import threading
 from typing import List, NamedTuple, Optional
 from scanners import *
@@ -106,7 +105,6 @@ def visualize_plot(args: argparse.Namespace, scan_data: DimScanData) -> None:
     if show_total_nonair and cumulative:
         print('Warning: ignoring flag "solids" because "cumulative" is set')
         show_total_nonair = False
-    dump_csv = args.dumpcsv  # TODO: do textual data output properly
 
     blocks_shown = load_block_selection(args.select)
 
@@ -139,7 +137,6 @@ def visualize_plot(args: argparse.Namespace, scan_data: DimScanData) -> None:
     
     fig, ax = plt.subplots(figsize=(8, 5))
 
-    graphs = []
     base_y = scan_data.base_y
     y_range = list(range(base_y, base_y + height))
     
@@ -161,9 +158,6 @@ def visualize_plot(args: argparse.Namespace, scan_data: DimScanData) -> None:
             hist_accum += hist
             graph = hist_accum
         
-        if dump_csv:
-            graphs.append((graph, display_name))
-        
         if color:
             ax.plot(y_range, graph, color, label=display_name)
         else:
@@ -173,13 +167,6 @@ def visualize_plot(args: argparse.Namespace, scan_data: DimScanData) -> None:
         nonair_hist = (chunks_scanned * 16**2) - sum_blocks_selection(scan_data, AIR_BLOCKS)
         graph = norm_func(nonair_hist)
         ax.plot(y_range, graph, 'lightgray', label='Non-air')
-        if dump_csv:
-            graphs.append((graph, 'Non-air'))
-
-    if dump_csv:
-        print(','.join(x[1] for x in graphs))
-        for graph in graphs:
-            print(','.join(map(str, graph[0])))
 
     ax.grid(True)
     ax.legend()
@@ -193,6 +180,49 @@ def visualize_plot(args: argparse.Namespace, scan_data: DimScanData) -> None:
         plt.savefig(args.savefig, dpi=300)
     else:
         plt.show()
+
+
+def vis_print_as_csv(args: argparse.Namespace, scan_data: DimScanData) -> None:
+    if args.layers:
+        crop_histogram(scan_data, args.layers)
+
+    if args.select == '*':
+        blocks_shown = None
+    else:
+        blocks_shown = load_block_selection(args.select)
+
+    height = scan_data.height
+    base_y = scan_data.base_y
+    y_range = list(range(base_y, base_y + height))
+    
+    table = []
+    if args.showy:
+        table.append(['Y'] + y_range)
+
+    if blocks_shown is not None:
+        for show_info in blocks_shown:
+            display_name = show_info.display_name or show_info.selectors
+            row = [display_name]
+            try:
+                hist = sum_blocks_selection(scan_data, map(parse_blockstate, show_info.selectors.split('+')))
+                print(f'{display_name} = {hist.sum()} blocks')
+                row.extend(hist)
+            except ValueError:
+                print(f'Found no blocks matching selectors: {show_info.selectors}')
+                row.extend([0] * height)
+            table.append(row)
+    else:
+        for name, idx in scan_data.blockstate_to_idx.items():
+            row = [name]
+            row.extend(scan_data.histogram[:, idx])
+            table.append(row)
+
+    if args.bylayer:
+        for graph in zip(*table):
+            print(*graph, sep=',')
+    else:
+        for graph in table:
+            print(*graph, sep=',')
 
 
 def operation_load_and_process(args: argparse.Namespace) -> None:
@@ -218,55 +248,58 @@ def operation_load_and_process(args: argparse.Namespace) -> None:
         visualize_print(args, scan_data)
     elif args.vismode == 'plot':
         visualize_plot(args, scan_data)
+    elif args.vismode == 'csv':
+        vis_print_as_csv(args, scan_data)
     elif args.vismode == 'save':
         operation_save(args, scan_data)
     else:
         raise InvalidScannerOperation('Unknown scan data processing action')
 
 
+def main():
+    def add_operation_parsers(parser: argparse.ArgumentParser) -> None:
+        subparsers = parser.add_subparsers(dest='vismode', required=True, help='Visualization choice')
+        
+        parse_vis_print = subparsers.add_parser('print', help='Print total block counts')
+        parse_vis_print.add_argument('--sort', choices=['id', 'count', 'name'], default='count', help='Block counts ordering')
+        
+        parse_vis_plot = subparsers.add_parser('plot', help='Plot histogram of block distribution')
+        parse_vis_plot.add_argument('select', help='Names of files containing selectors separated with ";"')
+        parse_vis_plot.add_argument('--norm', choices=['none', 'base', 'total', 'chunk'], default='total', help='Block counts ordering')
+        parse_vis_plot.add_argument('--solids', action='store_true', help='Display plot for non-air blocks')
+        parse_vis_plot.add_argument('--cumulative', action='store_true', help='Display as cumulative graph')
+        parse_vis_plot.add_argument('--layers', type=parse_dashed_range, default=None, help='Vertical range to display')
+        parse_vis_plot.add_argument('--savefig', default=None, help='Save plot to file instead of displaying in a window')
 
-def add_operation_parsers(parser: argparse.ArgumentParser) -> None:
-    subparsets = parser.add_subparsers(dest='vismode', required=True, help='Visualization choice')
-    
-    parse_vis_print = subparsets.add_parser('print', help='Write block counts to console')
-    parse_vis_print.add_argument('--sort', choices=['id', 'count', 'name'], default='count', help='Block counts ordering')
-    
-    parse_vis_plot = subparsets.add_parser('plot', help='Plot histogram of block distribution')
-    parse_vis_plot.add_argument('select', help='Names of files containing selectors separated with "+"')
-    parse_vis_plot.add_argument('--norm', choices=['none', 'base', 'total', 'chunk'], default='total', help='Block counts ordering')
-    parse_vis_plot.add_argument('--solids', action='store_true', help='Display plot for non-air blocks')
-    parse_vis_plot.add_argument('--cumulative', action='store_true', help='Display as cumulative graph')
-    parse_vis_plot.add_argument('--dumpcsv', action='store_true', help='Output graph data points as CSV')
-    parse_vis_plot.add_argument('--layers', type=parse_dashed_range, default=None, help='Vertical range to display')
-    parse_vis_plot.add_argument('--savefig', default=None, help='Save plot to file instead of displaying in a window')
+        parse_vis_csv = subparsers.add_parser('csv', help='Print selected block histograms as CSV')
+        parse_vis_csv.add_argument('select', help='Names of files containing selectors separated with ";" (use "*" to print everything)')
+        parse_vis_csv.add_argument('--layers', type=parse_dashed_range, default=None, help='Vertical range to use')
+        parse_vis_csv.add_argument('--bylayer', action='store_true', help='Print counts for each Y layer on a separate line')
+        parse_vis_csv.add_argument('--showy', action='store_true', help='Add Y level column to CSV')
 
-    parse_save_scan = subparsets.add_parser('save', help='Save extracted block histogram')
-    parse_save_scan.add_argument('output', help='Name of the new histogram file')
+        parse_save_scan = subparsers.add_parser('save', help='Save extracted block histogram')
+        parse_save_scan.add_argument('output', help='Name of the new histogram file')
 
-parser = argparse.ArgumentParser(description='Create a histogram of block distribution by height in a Minecraft world')
-action_parsers = parser.add_subparsers(dest='action', required=True, help='Action to perform')
+    parser = argparse.ArgumentParser(description='Create a histogram of block distribution by height in a Minecraft world')
+    action_parsers = parser.add_subparsers(dest='action', required=True, help='Action to perform')
 
-parse_load = action_parsers.add_parser('load', help='Load and display block histogram from file')
-parse_load.set_defaults(action_func=operation_load_and_process)
-parse_load.add_argument('statfile', help='Name of histogram file to display')
-add_operation_parsers(parse_load)
+    parse_load = action_parsers.add_parser('load', help='Load and display block histogram from file')
+    parse_load.add_argument('statfile', help='Name of histogram file to display')
+    add_operation_parsers(parse_load)
 
-parse_direct = action_parsers.add_parser('extract', help='Calculate block histogram for chosen world')
-parse_direct.set_defaults(action_func=operation_load_and_process)
-parse_direct.add_argument('world', help='Path to world directory')
-parse_direct.add_argument('--dim', default=0, type=int, help='ID of target dimension (default is overworld)')
-parse_direct.add_argument('--limit', type=int, default=None, help='Number of chunks to scan (default is all)')
-parse_direct.add_argument('--layers', type=parse_dashed_range, default=None, help='Vertial scan limits (default is 0-128)')
-add_operation_parsers(parse_direct)
+    parse_direct = action_parsers.add_parser('extract', help='Calculate block histogram for chosen world')
+    parse_direct.add_argument('world', help='Path to world directory')
+    parse_direct.add_argument('--dim', default=0, type=int, help='ID of target dimension (default is overworld)')
+    parse_direct.add_argument('--limit', type=int, default=None, help='Number of chunks to scan (default is all)')
+    parse_direct.add_argument('--layers', type=parse_dashed_range, default=None, help='Vertial scan limits (default is 0-128)')
+    add_operation_parsers(parse_direct)
 
-
-args = parser.parse_args()
-if hasattr(args, 'action_func'):
+    args = parser.parse_args()
     try:
-        args.action_func(args)
+        operation_load_and_process(args)
     except InvalidScannerOperation as err:
         print(err)
-    except Exception as err:
-        traceback.print_exc()
-else:
-    raise NotImplementedError(f'Action {args.action} doesn\'t have a handler')
+
+
+if __name__ == '__main__':
+    main()
