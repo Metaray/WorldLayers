ee127af6cfa45d5ba444242628992f80d2ce3bf1
Metaray <metaray21@gmail.com>
2022-07-04 14:19:59 +0300

    Add alpha world scanner
    Also improve automatic world version detection.

--- scanners.py
+++ scanners.py
@@ -1,8 +1,7 @@
+from pathlib import Path
 import numpy as np
 import uNBT as nbt
-from typing import Tuple
-import traceback
-import os
+from typing import Iterable, Tuple
 from time import perf_counter as clock
 from common import *
 from accelerators import *
@@ -10,13 +9,31 @@ from accelerators import *
 _CTR_DTYPE = np.int64 # Histogram counter type
 
 
+def iter_world_chunks(save_path: str, dim_id: int, world_type: str) -> Iterable[nbt.TagCompound]:
+    if world_type == 'alpha':
+        # Alpha format has same data as Region, but chunks are stored in individual files
+        # Numbers are base36 encoded
+        # 2 folder layers: first is X % 64, second is Z % 64
+        # Chunk file name: c.X.Y.dat
+        for file in Path(save_path).glob('*/*/c.*.*.dat'):
+            yield nbt.read_nbt_file(str(file))
+    
+    else:
+        for rfile_info in nbt.enumerate_world(save_path, world_type)[dim_id]:
+            print('Scanning', rfile_info.path)
+            region = nbt.Region.from_file(rfile_info.path)
+            for chunk in region.iter_nonempty():
+                yield chunk.nbt
+
+
 def scan_world_dimension(
     save_path: str,
     dim_id: int,
     scan_limit: int,
-    bounds: Tuple[int, int]
+    bounds: Tuple[int, int],
+    **_
 ) -> DimScanData:
-    """Scanner for versions >=1.2.1 <=1.12.2"""
+    """Scanner for versions >=1.2.1 <=1.12.2 (anvil format)"""
 
     ID_LIM = 2**12
     STATE_LIM = 16 * ID_LIM
@@ -41,33 +58,28 @@ def scan_world_dimension(
     ZERO_ADD = np.zeros(16**3, np.uint8)  # Filler for missing 'Add' or 'Add2'
     block_counts = np.zeros((height_limit, STATE_LIM), _CTR_DTYPE)
 
-    for rfile_info in nbt.enumerate_world(save_path)[dim_id]:
-        print('Scanning', rfile_info.path)
-        region = nbt.Region.from_file(rfile_info.path)
-        for chunk in region.iter_nonempty():
-            level_data = chunk.nbt['Level']
-            if not ('TerrainPopulated' in level_data and level_data['TerrainPopulated'].value):
+    for chunk_nbt in iter_world_chunks(save_path, dim_id, 'anvil'):
+        level_data = chunk_nbt['Level']
+        if not ('TerrainPopulated' in level_data and level_data['TerrainPopulated'].value):
+            continue
+        
+        for section in level_data['Sections']:
+            section_y = section['Y'].value
+            if not (min_section <= section_y < max_section):
                 continue
             
-            for section in level_data['Sections']:
-                section_y = section['Y'].value
-                if not (min_section <= section_y < max_section):
-                    continue
-                
-                # 16x16x16 arrays in YZX order
-                scan_v2_accel(
-                    block_counts,
-                    STATE_LIM,
-                    section_y - min_section,
-                    np.array(section['Blocks'].value, np.uint8),
-                    np.array(section['Add'].value, np.uint8) if 'Add' in section else ZERO_ADD,
-                    np.array(section['Add2'].value, np.uint8) if 'Add2' in section else ZERO_ADD,
-                    np.array(section['Data'].value, np.uint8),
-                )
+            # 16x16x16 arrays in YZX order
+            scan_v2_accel(
+                block_counts,
+                STATE_LIM,
+                section_y - min_section,
+                np.array(section['Blocks'].value, np.uint8),
+                np.array(section['Add'].value, np.uint8) if 'Add' in section else ZERO_ADD,
+                np.array(section['Add2'].value, np.uint8) if 'Add2' in section else ZERO_ADD,
+                np.array(section['Data'].value, np.uint8),
+            )
 
-            chunks_scanned += 1
-            if chunks_scanned >= scan_limit:
-                break
+        chunks_scanned += 1
         if chunks_scanned >= scan_limit:
             break
     
@@ -94,9 +106,10 @@ def scan_world_dimension_new(
     dim_id: int,
     scan_limit: int,
     bounds: Tuple[int, int],
-    save_properties: bool = True
+    save_properties: bool = True,
+    **_
 ) -> DimScanData:
-    """Scanner for versions 1.13+"""
+    """Scanner for versions 1.13+ (anvil format)"""
 
     # Set air index to zero for convinience
     blockstate_to_idx = {'minecraft:air': 0}
@@ -111,63 +124,58 @@ def scan_world_dimension_new(
     ZERO_LAYER = np.zeros((height_limit, 1), _CTR_DTYPE)
     index_map = np.zeros(16**3, np.uint32)
 
-    for rfile_info in nbt.enumerate_world(save_path)[dim_id]:
-        print('Scanning', rfile_info.path)
-        region = nbt.Region.from_file(rfile_info.path)
-        for chunk in region.iter_nonempty():
-            data_version = chunk.nbt['DataVersion'].value
-            level_data = chunk.nbt['Level'] if data_version < 2844 else chunk.nbt
-            if level_data['Status'].value not in STATUSES_READY:
+    for chunk_nbt in iter_world_chunks(save_path, dim_id, 'anvil'):
+        data_version = chunk_nbt['DataVersion'].value
+        level_data = chunk_nbt['Level'] if data_version < 2844 else chunk_nbt
+        if level_data['Status'].value not in STATUSES_READY:
+            continue
+        
+        has_carry = data_version < 2534
+        for section in level_data['Sections' if data_version < 2844 else 'sections']:
+            section_y = section['Y'].value
+            if not (min_section <= section_y < max_section):
                 continue
             
-            has_carry = data_version < 2534
-            for section in level_data['Sections' if data_version < 2844 else 'sections']:
-                section_y = section['Y'].value
-                if not (min_section <= section_y < max_section):
+            if data_version < 2836:
+                if 'Palette' not in section:
                     continue
+                states = section['BlockStates'].value
+                palette = section['Palette']
+            else:
+                if 'data' not in section['block_states']:
+                    continue
+                states = section['block_states']['data'].value
+                palette = section['block_states']['palette']
+            
+            for idx, block in enumerate(palette):
+                name = block['Name'].value
+                if save_properties and 'Properties' in block:
+                    # This must correctly keep in sync with `serialize_blockstate()`
+                    props = [(k, v.value) for k, v in block['Properties'].items()]
+                    props.sort()
+                    props = ','.join(f'{k}={v}' for k, v in props)
+                    name = f'{name}[{props}]'
                 
-                if data_version < 2836:
-                    if 'Palette' not in section:
-                        continue
-                    states = section['BlockStates'].value
-                    palette = section['Palette']
-                else:
-                    if 'data' not in section['block_states']:
-                        continue
-                    states = section['block_states']['data'].value
-                    palette = section['block_states']['palette']
-                
-                for idx, block in enumerate(palette):
-                    name = block['Name'].value
-                    if save_properties and 'Properties' in block:
-                        # This must correctly keep in sync with `serialize_blockstate()`
-                        props = [(k, v.value) for k, v in block['Properties'].items()]
-                        props.sort()
-                        props = ','.join(f'{k}={v}' for k, v in props)
-                        name = f'{name}[{props}]'
-                    
-                    try:
-                        bid = blockstate_to_idx[name]
-                    except KeyError:
-                        blockstate_to_idx[name] = bid = len(blockstate_to_idx)
-                        block_counts = np.hstack((block_counts, ZERO_LAYER))
-                    index_map[idx] = bid
-                
-                # `idx` holds maximum index in palette here
-                # `BlockStates` is packed 16x16x16 array in YZX order
-                scan_v13_accel(
-                    block_counts,
-                    block_counts.shape[1],
-                    section_y - min_section,
-                    np.array(states, np.int64),
-                    index_map,
-                    idx,
-                    has_carry,
-                )
+                try:
+                    bid = blockstate_to_idx[name]
+                except KeyError:
+                    blockstate_to_idx[name] = bid = len(blockstate_to_idx)
+                    block_counts = np.hstack((block_counts, ZERO_LAYER))
+                index_map[idx] = bid
+            
+            # `idx` holds maximum index in palette here
+            # `BlockStates` is packed 16x16x16 array in YZX order
+            scan_v13_accel(
+                block_counts,
+                block_counts.shape[1],
+                section_y - min_section,
+                np.array(states, np.int64),
+                index_map,
+                idx,
+                has_carry,
+            )
 
-            chunks_scanned += 1
-            if chunks_scanned >= scan_limit:
-                break
+        chunks_scanned += 1
         if chunks_scanned >= scan_limit:
             break
     
@@ -187,9 +195,9 @@ def scan_world_dimension_old(
     save_path: str,
     dim_id: int,
     scan_limit: int,
-    bounds: Tuple[int, int]
+    **_
 ) -> DimScanData:
-    """Scanner for versions <1.2.1"""
+    """Scanner for versions <1.2.1 (region format)"""
 
     # Hardcoded limits for old versions
     # TODO: old modpacks had ID extender mod (something analogous to 'Add' array in newer versions?)
@@ -201,24 +209,19 @@ def scan_world_dimension_old(
     chunks_scanned = 0
     block_counts = np.zeros((MAX_HEIGHT, STATE_LIM), _CTR_DTYPE)
 
-    for rfile_info in nbt.enumerate_world(save_path, 'region')[dim_id]:
-        print('Scanning', rfile_info.path)
-        region = nbt.Region.from_file(rfile_info.path)
-        for chunk in region.iter_nonempty():
-            level_data = chunk.nbt['Level']
-            if not ('TerrainPopulated' in level_data and level_data['TerrainPopulated'].value):
-                continue
+    for chunk_nbt in iter_world_chunks(save_path, dim_id, 'region'):
+        level_data = chunk_nbt['Level']
+        if not ('TerrainPopulated' in level_data and level_data['TerrainPopulated'].value):
+            continue
 
-            # 16x16x128 arrays in XZY order
-            scan_v0_accel(
-                block_counts,
-                np.array(level_data['Blocks'].value, np.uint8),
-                np.array(level_data['Data'].value, np.uint8),
-            )
+        # 16x16x128 arrays in XZY order
+        scan_v0_accel(
+            block_counts,
+            np.array(level_data['Blocks'].value, np.uint8),
+            np.array(level_data['Data'].value, np.uint8),
+        )
 
-            chunks_scanned += 1
-            if chunks_scanned >= scan_limit:
-                break
+        chunks_scanned += 1
         if chunks_scanned >= scan_limit:
             break
     
@@ -233,30 +236,64 @@ def scan_world_dimension_old(
     )
 
 
-def determine_scan_function(save_path: str, dim_id: int):
+def scan_world_dimension_alpha(
+    save_path: str,
+    scan_limit: int,
+    **_
+) -> DimScanData:
+    """Scanner for versions before beta 1.3 (infdev format)"""
+
+    # Hardcoded limits for old versions
+    STATE_LIM, MAX_HEIGHT = 256 * 16, 128
+
+    blockstate_to_idx = load_old_blockid_mapping()
+    chunks_scanned = 0
+    block_counts = np.zeros((MAX_HEIGHT, STATE_LIM), _CTR_DTYPE)
+
+    for chunk_nbt in iter_world_chunks(save_path, 0, 'alpha'):
+        level_data = chunk_nbt['Level']
+        if not ('TerrainPopulated' in level_data and level_data['TerrainPopulated'].value):
+            continue
+
+        # 16x16x128 arrays in XZY order
+        scan_v0_accel(
+            block_counts,
+            np.array(level_data['Blocks'].value, np.uint8),
+            np.array(level_data['Data'].value, np.uint8),
+        )
+
+        chunks_scanned += 1
+        if chunks_scanned >= scan_limit:
+            break
+    
+    block_counts, blockstate_to_idx, old_to_new = convert_to_new_bs_format(block_counts, blockstate_to_idx)
+
+    return DimScanData(
+        histogram=block_counts,
+        chunks_scanned=chunks_scanned,
+        blockstate_to_idx=blockstate_to_idx,
+        base_y=0,
+        old_to_new=old_to_new,
+    )
+
+
+def determine_scan_function(save_path: str):
     # Since 1.9 can use [level.dat].Data.DataVersion
     # First version after Flattening: 17w47a (data version: 1451)
     # 1.13 data version: 1519
-    level_data = nbt.read_nbt_file(os.path.join(save_path, 'level.dat'))['Data']
+    level_data = nbt.read_nbt_file(str(Path(save_path) / 'level.dat'))['Data']
     if 'DataVersion' in level_data and level_data['DataVersion'].value >= 1451:
         return scan_world_dimension_new
     
-    reg_dir = os.path.join(save_path, f'DIM{dim_id}' if dim_id else '', 'region')
-    if os.path.exists(reg_dir):
-        # For Anvil-Region detection scan for .mca files (they take priority)
-        for rfile in os.listdir(reg_dir):
-            if rfile.endswith('.mca'):
-                # Probably was converted. New version has priority.
-                return scan_world_dimension
+    # Anvil worlds have this tag
+    if 'initialized' in level_data:
+        return scan_world_dimension
+    
+    # Distinguish between alpha and region
+    if any(Path(save_path).rglob('*.mcr')):
         return scan_world_dimension_old
-
-    # TODO: Add Alpha format support
-    # Alpha format has same data as Region, but chunks are stored in individual files
-    # Numbers are base36 encoded
-    # 2 folder layers: first is X % 64, second is Z % 64
-    # Chunk file name: c.X.Y.dat
-
-    raise ValueError('Unable to determine world version')
+    else:
+        return scan_world_dimension_alpha
 
 
 def create_scan(
@@ -265,7 +302,7 @@ def create_scan(
     scan_limit: int,
     bounds: Tuple[int, int]
 ) -> DimScanData:
-    scan_func = determine_scan_function(save_path, dim_id)
+    scan_func = determine_scan_function(save_path)
     print(f'Using scanner: {scan_func.__name__}')
 
     t_start = clock()
@@ -286,6 +323,7 @@ def create_scan(
     t_finish = clock()
 
     print(f'Scanned {scan_data.chunks_scanned} chunks in {t_finish - t_start:.3f} seconds')
+    print(f'{scan_data.state_count} unique block states')
 
     scan_volume = scan_data.chunks_scanned * scan_data.height * 16**2
     nonair_count = scan_volume - sum_blocks_selection(scan_data, AIR_BLOCKS).sum()
